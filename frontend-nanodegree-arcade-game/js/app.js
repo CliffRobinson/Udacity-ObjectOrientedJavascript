2132687df2e4d8de6dddd8a1d1bb27fb178bc6a1
CliffRobinson <Clifford.f.robinson@gmail.com>
2018-09-19 14:11:58 +1200

    collision resets level

--- frontend-nanodegree-arcade-game/js/app.js
+++ frontend-nanodegree-arcade-game/js/app.js
@@ -3,8 +3,14 @@ const rightBound = 400;
 const topBound = -18;
 const bottomBound = 400;
 const bugwidth = 100;
+const initialPlayerX = 200;
 
+let allEnemies = [];
+let levelNum = 0;
 
+const enemyDist = [
+    [50],
+]
 
 // Enemies our player must avoid
 var Enemy = function(y) {
@@ -40,13 +46,21 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+
+function createEnemies(level) {
+    allEnemies = [];
+    enemyDist[level].map( (y) => allEnemies.push(new Enemy(y)) );
+}
+
+
+
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 
 var Player = function() {
     this.sprite = "images/char-boy.png";
-    this.x = 200;
+    this.x = initialPlayerX;
     this.y = bottomBound;
 };
 Player.prototype.render = function() {
@@ -94,14 +108,25 @@ Player.prototype.checkCollison = function(enemy) {
     }
 }
 Player.prototype.collision = function() {
-    console.log("BOOOOM!");
+    alert('You lose!'); 
+    resetLevel();
 }
+
+function resetLevel() {
+    player = new Player();
+    createEnemies(levelNum);
+}
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-const allEnemies = [new Enemy(50)];
-const player = new Player();
+createEnemies(levelNum);
+
+let player = new Player();
+
+alert(`Level ${levelNum +1}`);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
